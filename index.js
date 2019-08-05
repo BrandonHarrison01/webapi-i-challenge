e7edaad1151208b2924a8fde4ea229612f55b434
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-05 16:37:12 -0600

    get post and delete user functionality is working

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // implement your API here
+const express = require("express")
+
+const userData = require("./data/db")
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+server.get('/api/users', (req, res) => {
+    userData.find()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+
+    const newData = req.body;
+
+    console.log('new data', req.body)
+    userData.insert(newData)
+        .then(result => {
+            if (newData.name && newData.bio) {
+                res.status(201).json(result)
+            } else {
+                res.status(500).json({ errorMessage: "Please provide name and bio for the user." })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+
+    userData.remove(userId)
+        .then(result => {
+            if (userId) {
+                res.status(200).json(result)
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The user could not be removed" })
+        })
+})
+
+
+
+const port = 8000;
+server.listen(port, () => console.log('api running'))
